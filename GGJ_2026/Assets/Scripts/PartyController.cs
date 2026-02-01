b02e599ca5e2cf691f0cad741790c8a48a8e6551
SnowyOchole <annabelleochotta@gmail.com>
2026-02-01 18:30:13 -0500

    THE DAY IS SEPARATE

--- GGJ_2026/Assets/Scripts/PartyController.cs
+++ GGJ_2026/Assets/Scripts/PartyController.cs
@@ -9,7 +9,6 @@ using UnityEngine.SceneManagement;
 
 public class PartyController : MonoBehaviour
 {
-    static public int day = 0;
     public enum Species
     {
         Vampire,
@@ -17,7 +16,6 @@ public class PartyController : MonoBehaviour
         Fae,
         Siren
     };
-    bool b = false;
 
     [SerializeField] GameObject npc;
     int[] numOfNPCs = { 20, 28, 35 };
@@ -31,7 +29,6 @@ public class PartyController : MonoBehaviour
 
     [SerializeField] public static int partyType;
     //stairs vs balcony height when sprites are in
-
     
     [SerializeField] float posXBound = 9.5f, negXBound = -9.0f;
     float[] yLevels = { 4.2f, -2f, -4.5f, -7f};
@@ -49,7 +46,6 @@ public class PartyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this);
         InitializePossibleGuestLocations();
         RandomizeGuests();
         OrganizeSpecies();
@@ -73,7 +69,7 @@ public class PartyController : MonoBehaviour
         float xDist = posXBound - negXBound;
         Debug.Log(xDist);
         
-        switch (day)
+        switch (Day.day)
         {
             case 0://7753
                 spotsPerY[0] = 7;
@@ -135,12 +131,12 @@ public class PartyController : MonoBehaviour
 
         //for now, random
         partyType = _speciesLeft[0];
-        int _guestsRemaining = numOfNPCs[day];
+        int _guestsRemaining = numOfNPCs[Day.day];
 
         //for eaach species but not always in the same order
 
         //pick from the species and then assign an amount of attendees
-        int _firstSpecies = UnityEngine.Random.Range((int)(0.5 * numOfNPCs[day]), (int)(0.7 * numOfNPCs[day]));
+        int _firstSpecies = UnityEngine.Random.Range((int)(0.5 * numOfNPCs[Day.day]), (int)(0.7 * numOfNPCs[Day.day]));
         attendeeDistribution[partyType] = _firstSpecies;
         _guestsRemaining -= _firstSpecies;
         //Debug.Log(DebugRandom(_firstSpecies, partyType, _guestsRemaining));
@@ -157,7 +153,7 @@ public class PartyController : MonoBehaviour
             }
             else
             {
-                _currSpeciesPercent = UnityEngine.Random.Range((int)(0.1 * numOfNPCs[day]), (int)(_guestsRemaining * 0.75));
+                _currSpeciesPercent = UnityEngine.Random.Range((int)(0.1 * numOfNPCs[Day.day]), (int)(_guestsRemaining * 0.75));
               
             }
             _guestsRemaining -= _currSpeciesPercent;
@@ -211,7 +207,7 @@ public class PartyController : MonoBehaviour
 
         UnityEngine.Random.InitState(System.DateTime.Now.Second + (int)System.DateTime.Now.Ticks);
         List<int> spotsTaken = new List<int>();
-        while(numOfGroupsLeft > 0)//per each day
+        while(numOfGroupsLeft > 0)//per each Day.Day.day
         {
             int spot_to_check = UnityEngine.Random.Range(0, totalSpots);
             //draw number
@@ -407,20 +403,11 @@ public class PartyController : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            
-        }
 
         if(partyEnergy >= 70)
         {
-            day++;
-            SceneManager.LoadScene("Carmila VN");
+            Day.day++;
+            SceneManager.LoadScene("Carmilla VN");
         }
     }
 }
